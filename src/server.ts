d0c27a6f6ee2e17bcbf55c89c51a2b4333362aa7
IkromovOtabek <otabekikromov32@gmail.com>
2026-03-26 19:46:04 +0900

    EXPRESS

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("salom");
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import app from "./app";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log(`Muvaffaqiyatli ulandi gazini BOOS~`);
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function() {
+        console.log(`The server is running seccessfully on port: ${PORT}`);
+    })
+  })
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
